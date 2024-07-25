7b6d04deb06315f212ab8a626f6c016e0f8a6523
Ujsharma <ujwalsharma.s@gmail.com>
2024-07-25 18:42:30 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,13 @@
-// main.cpp
 #include <iostream>
-#include <cassert>
-#include "colorcoder.h"
-#include "NumPairConversion.hpp"
+#include "test_colorcoder.hpp"
 
-// Main function with test cases
 int main() {
-    
-    // Original test cases
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
     testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
 
+    std::cout << "All tests passed!" << std::endl;
     return 0;
 }
