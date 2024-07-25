e0aa94290cdd50e4fa6e0e2f19f7d29217ff6456
Ujsharma <ujwalsharma.s@gmail.com>
2024-07-25 14:08:18 +0530

    Update NumPairConversion.hpp

--- NumPairConversion.hpp
+++ NumPairConversion.hpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <assert.h>
-#include "GetColorCode.hpp"
+#include "ColorCoder.h"
 
 void testNumberToPair(int pairNumber,
     TelCoColorCoder::MajorColor expectedMajor,
