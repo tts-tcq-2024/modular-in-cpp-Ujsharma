b1a5be3b0a5a86d7b5f90c5bb4af66bebf0d75b8
Ujsharma <ujwalsharma.s@gmail.com>
2024-07-25 14:20:08 +0530

    Update NumPairConversion.hpp

--- NumPairConversion.hpp
+++ NumPairConversion.hpp
@@ -6,7 +6,6 @@ void testNumberToPair(int pairNumber,
     TelCoColorCoder::MajorColor expectedMajor,
     TelCoColorCoder::MinorColor expectedMinor);
 
-void testPairToNumber(
-    TelCoColorCoder::MajorColor major,
-    TelCoColorCoder::MinorColor minor,
-    int expectedPairNumber);
+void testPairToNumber(TelCoColorCoder::MajorColor major,
+                          TelCoColorCoder::MinorColor minor,
+                          int expectedPairNumber);
